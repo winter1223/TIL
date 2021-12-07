46cb0a4e07a6ef6826b8010d0b34cdd769550635
winter <sara961223@gmail.com>
2021-12-08 08:52:02 +0900

    pythonpractice

---
+++ python.py/pracitce.py
@@ -0,0 +1,39 @@
+print('hello')
+#자료형
+##숫자형자료형
+print(5)
+print(25) 
+print('ddldkfj')
+print(-10)
+print(3.14)
+print(10000000)
+print(5+3)
+print(2*2)
+print(3*(12*1))
+##문자형
+print('냠냠')
+print('삼겹살 먹고싶다')
+print('크킄'*10)
+##불리언 (참 거짓)
+print(1>10)
+print(True)
+print(not False)
+print(not (5<10))
+print(not (5>10))
+##변수
+###변수는 어떤 값을 저장하는 공간을 말함
+
+print('내가 잠시 동거한 고양이는 일로와예요')
+print('일로와는 너무  귀여워요')
+print( '일로와는 3살 뚱냥이예요')
+
+
+name = '일로와'
+ani = '고양이'
+age = 3
+hob = '귀여워'
+is_adult = age >=1
+
+print('내가 잠시 동거한' +ani + ' 는 ' + name + '예요')
+print(name + '는 너무 ' + hob + '요')
+print( name  +  str ( age ) + '살 뚱냥이예요')
